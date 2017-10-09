ffd15b90907a669603a92c86dc280ee66a55f07d
Godfred Nkumane <Godfred.Nkumane@ssp-worldwide.com>
2017-10-09 14:35:38 +0200

    Connit

---
+++ WebAPI/User/Main.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="C:\Users\godfred.nkumane\Documents\Visual Studio 2015\Projects\WebAPI\WebAPI\Scripts/angular.js" />
+
+var webAPI = angular.module('firstApplication', ['ngMaterial'])
+                    .controller('inputController', inputController);
+
+function inputController($scope) {
+    $scope.project = {
+        comments: 'Comments',
+    };
+}
